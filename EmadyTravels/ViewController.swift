db27136c057694d45d46343f5a08a8c2fe102050
Hiten Patel <Hiten@hitens-macbook-pro.local>
2019-03-13 10:31:21 -0400

    Initial Commit

---
+++ EmadyTravels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmadyTravels
+//
+//  Created by Hiten Patel on 2019-03-13.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
